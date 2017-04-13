f15b37730be8778828b4f36693a878ffda843963
ccpeterson <clinton.c.peterson@gmail.com>
2017-04-12 18:14:21 -0700

    turtle final changes

--- as05/petersonturtle.py
+++ as05/petersonturtle.py
@@ -8,7 +8,7 @@ import turtle
 # Setup window
 win = turtle.Screen()
 win.setup(500, 500)
-win.title("Peterson Turtle")
+win.title("Peterson Turtle! VOTE FOR ME!")
 win.bgcolor("white")
 
 # Setup the turtle attributes
@@ -16,12 +16,12 @@ pete = turtle.Turtle()
 pete.shape("turtle")
 pete.fillcolor("green")
 pete.pencolor("black")
-pete.speed(1000)
+pete.speed(100)
 
 #loop 60 times
 #this number and the turn at the end need to have a product of 360 to make a full circle
 for i in range(60):
-#make  a crazy line out from center
+#make a crazy line out from center
     pete.forward(100)
     pete.right(40)
     pete.forward(50)
@@ -37,13 +37,28 @@ for i in range(60):
 
 #pander for votes    
 pete.penup()
-pete.setposition(-200, 200)
+pete.setposition(-150, 200)
 pete.fillcolor("red")
 pete.pencolor("blue")
 pete.write("VOTE PETERSON!!!", align="left", font=("Arial", 24, "normal"))
 
+#do a little dance
+for i in range(20):
+    pete.right(45)
+    pete.forward(10)
+    pete.left(90)
+    pete.forward(10)
+    pete.right(45)
+
+#another stationary color dance
+for i in range(20):
+    pete.fillcolor("yellow")
+    pete.right(45)
+    pete.fillcolor("pink")
+    pete.right(45)    
+
 # Where is Pete?
-print(pete.xcor())
-print(pete.ycor())
-print(pete.heading())
+print("Little Pete's final loaction: X", pete.xcor(),"Y", pete.ycor(), "Heading", pete.heading())
+print()
+print("VOTE FOR PETERSON!")
 
