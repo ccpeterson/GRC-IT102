f005475f90ee046b51428d05ade949471886db33
ccpeterson <clinton.c.peterson@gmail.com>
2017-04-12 18:21:48 -0700

    finished magic 8 ball

--- as04/magic.py
+++ as04/magic.py
@@ -9,12 +9,14 @@ import random
 #possible answers
 possible = ("It is certain", "It is decidedly so", "Without a doubt", "Yes definitely", "You may rely on it", "As I see it, yes", "Most likely", "Outlook good", "Yes", "Signs point to yes", "Reply hazy try again", "Ask again later", "Better not tell you now", "Cannot predict now", "Concentrate and ask again", "Don't count on it", "My reply is no", "My sources say no", "Outlook not so good", "Very doubtful")
 
+#welcome prompt
+input("Welcome to the super-accurate MAGIC 8 BALL. To begin press enter.")
 
-#extra points with a loop!
+#extra points four a loop
 playAgain = "yes"
 while playAgain == "yes":
 #Asking the question
-	question = input ("Please ask a yes or no question")
+	question = input ("Ask me any question that can answered with a yes or no. ")
 #this is where I get extra extra points	
 	print("The magic 8 ball is thinking")
 	print("         ____")
@@ -32,14 +34,15 @@ while playAgain == "yes":
 	print("")
 	input("Press enter to discover the answer")
 #spitting out the answer
+	print("The answer to your very deep question,(", question, ") is....")
 	answer = random.choice(possible)
-	print("The magic 8-Ball says....." + answer)
+	print(answer)
 	print("")
 #asking if they want to continue
 	print("Do you want to consult the 8 ball again? (yes or no)")
 	playAgain = input()
 
-print("ok, bye")	
+print("Have a mystical day!")	
 input()
 	
 		
