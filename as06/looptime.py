916fae700ffcfb01792c2678ca3e303f4782e938
ccpeterson <clinton.c.peterson@gmail.com>
2017-04-18 19:45:06 -0700

    class

--- as06/looptime.py
+++ as06/looptime.py
@@ -5,6 +5,13 @@
 
 # First scenario
 
-# Second scenario
-
-# Third scenario
+import random
+colorList = ["red","blue","green","yellow","black","white","purple","brown","gray","magenta","orange","violet","pink","turquoise","sky blue","indigo","gold","silver","maroon","coral","salmon","tan","lime","teal","beige"]
+print("I will print you a random list of colors")
+userChoice = input("How long would you like the list? Input a number from 1 - 20: ")
+#make it an int
+userChoice=int(userChoice)
+#loop it up
+for i in range (userChoice): #the range will make the number of colors printed the same as the user input
+    print (i)
+	
