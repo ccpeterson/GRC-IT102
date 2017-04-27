2f7e15c28773bd84a0cb2185a96b6b1f1300ce91
ccpeterson <clinton.c.peterson@gmail.com>
2017-04-26 20:01:26 -0700

    finishing median

--- as09/median.py
+++ as09/median.py
@@ -1,33 +1,41 @@
 # median.py
-# linding the median of a set of numbers
+# a function that finds the median of a set of numbers
 # Clinton Peterson
 # 25 April 2017
 
-#Here's the function
+#Here's the function, it expects a list of numbers
 def median(listName):
 #sorting the list
     listName = sorted(listName)
 #Testing that it's sorted 
-    print(listName)
-#doing the median of a list if it's of even length
+    print("Here's the list, but sorted.",listName)
+#Finding the median of the list if it's of even length
+#Gotta find the two middle numbers and average them
     if (len(listName)%2 == 0):
         evenMed1 = listName[(len(listName)//2)-1]
         evenMed2 = listName[(len(listName)//2)]
         evenMed = (evenMed1 + evenMed2)/2
-        print(evenMed)
-#if it's not even it's odd, so even easier
+        print("The Median of that list is",evenMed)
+#If it's not even it's odd, so even easier
+#Just finding the middle number
     else:
         oddMed = listName[(len(listName)//2)]
-        print(oddMed)
+        print("The Median of that list is",oddMed)
 
-#Here's the list
-thing = [6,4,2,5,3,1,7,9,10,56]
-#Why not print it first so I can see that it was sorted
-print(thing)
+#Here's two lists
+thingEven = [6,4,2,5,3,9,10,56]
+thingOdd = [6,4,7,2,5,3,1,7,9,106,99]
+
+#showing the list of numbers before we run it through the function
+print("Here's the list of numbers from the odd numbered list",thingOdd)
 #doing the thing I came to do
-median(thing)
+median(thingOdd)
 
+print()
 
-
+#Do it again but with a different list
+print("Here's the list of numbers from the even numbered list",thingEven)
+#doing the thing I came to do
+median(thingEven)
 	
 	
