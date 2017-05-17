9424cb5a6e5ec611bde4c4a3ef562b987e411266
ccpeterson <clinton.c.peterson@gmail.com>
2017-05-17 14:55:31 -0700

    flappy work

--- as12/threeBallBounce.py
+++ as12/threeBallBounce.py
@@ -7,10 +7,11 @@ from graphics import *
 import random
 
 #Define Constants
-WIN_WIDTH = 800   #Pixels
-WIN_HEIGHT = 800  #Pixels
+WIN_WIDTH = 800 #Size of window in pixels
+WIN_HEIGHT = 800 #Size of window in pixels
 UPDATE_RATE = 60 #Frames per second
-BALL_RAD = 50
+BALL_RAD = 50 #Size of ball in pixels
+MOVEX = 5 #Movement per frame in pixels
 
 
 #Setup graphics window object
@@ -18,7 +19,6 @@ win = GraphWin("GraphicsWindow", WIN_WIDTH, WIN_HEIGHT, autoflush=False)
 win.setBackground("grey")
 
 #Setup up the balls
-
 ball01 = Circle(Point(WIN_WIDTH//2 - 100,WIN_HEIGHT/2), BALL_RAD)
 ball01.setFill("red")
 ball01.setOutline("black")
@@ -37,38 +37,39 @@ ball03.setOutline("black")
 ball03.setWidth(5)
 ball03.draw(win)
 
-moveX = 5
-moveY = 5
-
+#Initial direction of the movement
 xDir1 = -1
 xDir2 = 1
 xDir3 = -1
 
 #Main Program
 while True:
+    #If Ball 1's x cord is within a radius of the wall, reverse its direction
     if ball01.getCenter().getX() > WIN_WIDTH-BALL_RAD or ball01.getCenter().getX() < BALL_RAD:
         xDir1 *= -1        
 
+    #If Ball 2's x cord is within a radius of the wall, reverse its direction
     if ball02.getCenter().getX() > WIN_WIDTH-BALL_RAD or ball02.getCenter().getX() < BALL_RAD:
         xDir2 *= -1
 
+    #If Ball 3's x cord is within a radius of the wall, reverse its direction
     if ball03.getCenter().getX() > WIN_WIDTH-BALL_RAD or ball03.getCenter().getX() < BALL_RAD:
         xDir3 *= -1        
-    
+
+    #If ball 1 and 2's x cords are within 2 radius of each other, switch both of their directions
     if abs(ball01.getCenter().getX() - ball02.getCenter().getX()) < 2*BALL_RAD:
         xDir1 *= -1
         xDir2 *= -1
-        
+
+    #If ball 3 and 2's x cords are within 2 radius of each other, switch both of their directions    
     if abs(ball03.getCenter().getX() - ball02.getCenter().getX()) < 2*BALL_RAD:
         xDir3 *= -1
         xDir2 *= -1
-    
-    ball01.move(moveX*xDir1, 0)
-    ball02.move(moveX*xDir2, 0)
-    ball03.move(moveX*xDir3, 0)
-    
+
+    #Move the balls
+    ball01.move(MOVEX*xDir1, 0)
+    ball02.move(MOVEX*xDir2, 0)
+    ball03.move(MOVEX*xDir3, 0)
+
+    #Draw the screen
     update(UPDATE_RATE)
-
-
-
-
--- as13/flappyBall.py
+++ as13/flappyBall.py
@@ -8,25 +8,26 @@ import random
 #Define Constants
 WIN_WIDTH = 800   #Pixels
 WIN_HEIGHT = 800  #Pixels
-UPDATE_RATE = 60 #Frames per second
-BALL_RAD = 20 #Ball Size in Pixels
-MOVEX = 1 #moving left speed
+UPDATE_RATE = 60 #frames per second
+BALL_RAD = 30 #ball Size in Pixels
+MOVEX = 2 #pipe's moving left speed
 MOVEY = 2 #falling speed
-PIPE_LENGTH = 300
-PIPE_WIDTH = 50
+PIPE_LENGTH = 30\0 #in pixels, from floor and ceiling
+PIPE_WIDTH = 50 #in pixels
 
 
 #Setup graphics window object
-win = GraphWin("GraphicsWindow", WIN_WIDTH, WIN_HEIGHT, autoflush=False)
+win = GraphWin("Flappy Ball", WIN_WIDTH, WIN_HEIGHT, autoflush=False)
 win.setBackground("grey")
 
-#Setup up the ball and pipes
-ball01 = Circle(Point(100 ,WIN_HEIGHT/2), BALL_RAD)
+#Setup up the ball
+ball01 = Circle(Point(200 ,WIN_HEIGHT/2), BALL_RAD)
 ball01.setFill("blue")
 ball01.setOutline("black")
 ball01.setWidth(5)
 ball01.draw(win)
 
+#Setup the pipes
 tRect01 = Rectangle(Point(WIN_WIDTH-PIPE_WIDTH,0), Point(WIN_WIDTH,PIPE_LENGTH))
 tRect01.setFill("blue")
 tRect01.setOutline("black")
@@ -36,34 +37,44 @@ bRect01.setFill("blue")
 bRect01.setOutline("black")
 bRect01.draw(win)
 
+#setting variables to be changed when you win or lose
 gameOver = 0
 winner = 0
+
+#This sets a variable to be the same as the x cord of the leading edge of the pipe
 xPipe = WIN_WIDTH - PIPE_WIDTH
+
 #Main Program
 while True:
     key = win.checkKey()
+    #for every space bar it flaps up 20 times what it falls each frame
     if key == "space":
         ball01.move(0, -20 * MOVEY)
+    #if the ball hits the bottom, you lose
     if ball01.getCenter().getY() > WIN_HEIGHT - BALL_RAD :
         gameOver = 1
-    if xPipe == 0:
+    #if the pipe reaches the left side, you win
+    if xPipe <= 0:
         winner = 1
-    if ball01.getCenter().getX() >= xPipe :
-        if ball01.getCenter().getY()  < PIPE_LENGTH or ball01.getCenter().getY()   > WIN_HEIGHT-PIPE_LENGTH :
+    #if the ball's xcord (+- the radius of the ball) is within the pipe's range check the nested if
+    if ball01.getCenter().getX() + BALL_RAD >= xPipe and ball01.getCenter().getX() - BALL_RAD <= xPipe + PIPE_WIDTH :
+        #if the ball's ycord (+- the radius of the ball) is within the pipe's range, game over
+        if ball01.getCenter().getY() - BALL_RAD <= PIPE_LENGTH or ball01.getCenter().getY() + BALL_RAD >= WIN_HEIGHT-PIPE_LENGTH:
             gameOver = 1        
+    #if gameover has been triggered, turn the ball red and break the loop
     if gameOver == 1 :
         ball01.setFill("red")
         break
+    #if winner has been triggered, turn the ball 
     if winner == 1:
         ball01.setFill("green")
         break
-    
+    #make the ball fall
     ball01.move(0, MOVEY)
+    #make both rectangles move left
     tRect01.move(-MOVEX, 0)
     bRect01.move(-MOVEX, 0)
+    #advance the counter that is equal the x cord of the leading edge of the pipe
     xPipe = xPipe - MOVEX
+    #refresh the screen
     update(UPDATE_RATE)
-
-
-
-
