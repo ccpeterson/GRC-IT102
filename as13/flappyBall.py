998a3f97dae94bf5e5477bcbd3dbc14aff0c346c
ccpeterson <clinton.c.peterson@gmail.com>
2017-05-17 15:55:36 -0700

    flappy work

--- as13/flappyBall.py
+++ as13/flappyBall.py
@@ -46,26 +46,27 @@ xPipe = WIN_WIDTH - PIPE_WIDTH
 
 #Main Program
 while True:
+    #Setting a variable for key presses
     key = win.checkKey()
-    #for every space bar it flaps up 20 times what it falls each frame
+    #For every space bar it flaps up 20 times what it falls each frame
     if key == "space":
         ball01.move(0, -20 * MOVEY)
-    #if the ball hits the bottom, you lose
+    #If the ball hits the bottom, gameover
     if ball01.getCenter().getY() > WIN_HEIGHT - BALL_RAD :
         gameOver = 1
-    #if the pipe reaches the left side, you win
+    #If the pipe reaches the left side, winner
     if xPipe <= 0:
         winner = 1
-    #if the ball's xcord (+- the radius of the ball) is within the pipe's range check the nested if
+    #If the ball's xcord (+- the radius of the ball) is within the pipe's range check the nested if
     if ball01.getCenter().getX() + BALL_RAD >= xPipe and ball01.getCenter().getX() - BALL_RAD <= xPipe + PIPE_WIDTH :
-        #if the ball's ycord (+- the radius of the ball) is within the pipe's range, game over
+        #if the ball's ycord (+- the radius of the ball) is within the pipe's range, gameover
         if ball01.getCenter().getY() - BALL_RAD <= PIPE_LENGTH or ball01.getCenter().getY() + BALL_RAD >= WIN_HEIGHT-PIPE_LENGTH:
             gameOver = 1        
-    #if gameover has been triggered, turn the ball red and break the loop
+    #If gameover has been triggered, turn the ball red and break the loop
     if gameOver == 1 :
         ball01.setFill("red")
         break
-    #if winner has been triggered, turn the ball 
+    #If winner has been triggered, turn the ball green and break the loop
     if winner == 1:
         ball01.setFill("green")
         break
